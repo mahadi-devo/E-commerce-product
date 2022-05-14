338d6b2f33d1f0c1d04f6c21dc548e7d5541e197
mahadi-devo <mahadihassan083@gmail.com>
2022-05-14 18:27:46 +0530

    initial product commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {Button, Card} from "react-bootstrap";
 
 function App() {
+
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/1')
+        .then(res=>res.json())
+        .then(products=>setProduct(products))
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1 className="p-5 text-center">{product.title}</h1>
+        <div className="d-flex">
+          {
+
+                <Card className="m-2" style={{ width: '18rem' }} key={product.id}>
+                  <Card.Img variant="top" src={product.image} />
+                  <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>
+                      {product.description}
+                    </Card.Text>
+                    <Button variant="primary">Buy {product.price}</Button>
+                  </Card.Body>
+                </Card>
+          }
+          <div className="badge bg-success">
+              { product.rating && product.rating.rate}
+          </div>
+        </div>
+      </div>
   );
 }
 
